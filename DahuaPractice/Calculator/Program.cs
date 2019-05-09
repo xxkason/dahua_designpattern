d3c92c4f1e59480da869918b2d4f6e9d3485ac6c
Kai Peng <xxkason@outlook.com>
2019-05-09 23:15:31 +0800

    simple factory pattern

--- DahuaPractice/Calculator/Program.cs
+++ DahuaPractice/Calculator/Program.cs
@@ -2,23 +2,117 @@
 
 namespace calculator
 {
-    class Operation
+    public class Operation
     {
-        public static double GetResult(double Num1, double Num2, string opt)
+        private double _numA = 0;
+        private double _numB = 0;
+
+        public double NumA
         {
+            get { return _numA; }
+            set { _numA = value; }
+        }
+
+        public virtual double NumB
+        {
+            get { return _numB; }
+            set { _numB = value; }
+        }
+
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+        // public virtual double GetResult(double Num1, double Num2, string opt)
+        // {
+        //     switch (opt)
+        //     {
+        //         case "+":
+        //             return Num1 + Num2;
+        //         case "-":
+        //             return Num1 - Num2;
+        //         case "*":
+        //             return Num1 * Num2;
+        //         case "/":
+        //             return Num1 / Num2;
+        //         default:
+        //             throw new Exception("Unsupported operation");
+        //     }
+        // }
+    }
+
+    public class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            return NumA + NumB;
+        }
+    }
+
+    public class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            return NumA - NumB;
+        }
+    }
+
+    public class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            return NumA * NumB;
+        }
+    }
+
+    public class OperationDiv : Operation
+    {
+        private double _numB = 0;
+        public override double NumB
+        {
+            get { return _numB; }
+            set
+            {
+                if (value == 0.0)
+                {
+                    throw new Exception("invalid parameter");
+                }
+                else
+                {
+                    _numB = value;
+                }
+            }
+        }
+        public override double GetResult()
+        {
+            return NumA / NumB;
+        }
+    }
+
+    public class OperationFactory
+    {
+        public static Operation CreateOperation(string opt)
+        {
+            Operation oper = null;
             switch (opt)
             {
                 case "+":
-                    return Num1 + Num2;
+                    oper = new OperationAdd();
+                    break;
                 case "-":
-                    return Num1 - Num2;
+                    oper = new OperationSub();
+                    break;
                 case "*":
-                    return Num1 * Num2;
+                    oper = new OperationMul();
+                    break;
                 case "/":
-                    return Num1 / Num2;
+                    oper = new OperationDiv();
+                    break;
                 default:
                     throw new Exception("Unsupported operation");
             }
+            return oper;
         }
     }
 
@@ -32,10 +126,13 @@ namespace calculator
             string opt = Console.ReadLine();
             Console.Write("Please input the second number: ");
             string secondNumber = Console.ReadLine();
-            double result;
             try
             {
-                result = Operation.GetResult(Convert.ToDouble(firstNumber), Convert.ToDouble(secondNumber), opt);
+                Operation oper = OperationFactory.CreateOperation(opt);
+                oper.NumA = Convert.ToDouble(firstNumber);
+                oper.NumB = Convert.ToDouble(secondNumber);
+                double result = oper.GetResult();
+                // result = Operation.GetResult(Convert.ToDouble(firstNumber), Convert.ToDouble(secondNumber), opt);
                 // switch (opt)
                 // {
                 //     case "+":
