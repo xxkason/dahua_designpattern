ba460de52dbf3e3892c873f0ed921a59953d698e
Kai Peng <xxkason@outlook.com>
2019-05-12 22:23:42 +0800

    strategy pattern

---
+++ DahuaPractice/cashier/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace cashier
+{
+    public abstract class DiscountSuper
+    {
+        public abstract double GetDiscountResult(double total);
+    }
+
+    public class DiscountRebate : DiscountSuper
+    {
+        private double _discountRate = 1;
+        public DiscountRebate(double discountRate)
+        {
+            _discountRate = discountRate;
+        }
+
+        public override double GetDiscountResult(double total)
+        {
+            return total * _discountRate;
+        }
+    }
+
+    public class DiscountReturn : DiscountSuper
+    {
+        private double _base = 0;
+        private double _return = 0;
+        public DiscountReturn(double moneyBase, double moneyReturn)
+        {
+            this._base = moneyBase;
+            this._return = moneyReturn;
+        }
+
+        public override double GetDiscountResult(double money)
+        {
+            return money - Math.Floor(money / _base) * _return;
+        }
+    }
+
+    public class DiscountFactory
+    {
+        public static DiscountSuper DiscountActivity(int index)
+        {
+            switch (index)
+            {
+                case 1:
+                    return new DiscountRebate(0.8);
+                case 2:
+                    return new DiscountReturn(100, 50);
+                default:
+                    return new DiscountRebate(1);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dTotal = 0.0;
+            int index = 0;
+            while (Console.ReadLine() != "=")
+            {
+                index++;
+                Console.Write("Please input the unit price of the good: ");
+                string strUnitPrice = Console.ReadLine();
+                double dUnitPrice = Convert.ToDouble(strUnitPrice);
+                Console.Write("Please input the number of the good: ");
+                string strAmount = Console.ReadLine();
+                int iAmount = Convert.ToInt32(strAmount);
+                Console.Write("Please select discount activity: 0 Normal, 1 八折, 2 满100返50");
+                string strDiscount = Console.ReadLine();
+                int iDiscount = Convert.ToInt16(strDiscount);
+                DiscountSuper activity = DiscountFactory.DiscountActivity(iDiscount);
+                double dUnitTotal = activity.GetDiscountResult(dUnitPrice * iAmount);
+                dTotal = dTotal + dUnitTotal;
+                string msg = String.Format("{4}: Unit Price: ${0:F}, Number: {1}, Total: {2:F}\nCurrent Total: ${3:F}", dUnitPrice, iAmount, dUnitTotal, dTotal, index);
+                Console.WriteLine(msg);
+            }
+            Console.WriteLine("You should pay: ${0:F}", dTotal);
+        }
+    }
+}
